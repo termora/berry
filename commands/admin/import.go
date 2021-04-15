fac36272b0f4b8301053bee6fc906e8179ee0032
Starshine System <us@starshines.xyz>
2021-04-15 14:18:05 +0200

    make import regexes case-insensitive

--- commands/admin/import.go
+++ commands/admin/import.go
@@ -11,9 +11,9 @@ import (
 	"github.com/termora/berry/db"
 )
 
-var msgRegex = regexp.MustCompile(`\*\*Name:?\*\*:?(.*)\n\*\*Category:?\*\*:?(.*)\n\*\*Description:?\*\*:?([\s\S]*)\n\*\*Coined by:?\*\*:?(.*)`)
+var msgRegex = regexp.MustCompile(`(?i)\*\*Name:?\*\*:?(.*)\n\*\*Category:?\*\*:?(.*)\n\*\*Description:?\*\*:?([\s\S]*)\n\*\*Coined by:?\*\*:?(.*)`)
 
-var tagsRegex = regexp.MustCompile(`\*\*Tags:\*\* (.*)`)
+var tagsRegex = regexp.MustCompile(`(?i)\*\*Tags:\*\* (.*)`)
 
 func (c *Admin) importFromMessage(ctx *bcr.Context) (err error) {
 	flag, _ := ctx.Flags.GetString("category")
